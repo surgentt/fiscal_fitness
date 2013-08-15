119e4f499fbdef64ee10e7a145fe4a453fe755d1
Thomas Surgent <surgentt@gmail.com>
2013-08-15 16:47:25 -0400

    FInish Layout and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,42 +2,27 @@ require 'spec_helper'
 
 describe "Static pages" do
 
+  subject { page }
+
   describe "Home page" do
+    before { visit root_path }
 
-    it "should have the content 'Fiscal Fitness'" do
-      visit '/static_pages/home'
-      expect(page).to have_content('Fiscal Fitness')
-    end
-
-    it "should have the title 'Home'" do
-      visit '/static_pages/home'
-      expect(page).to have_title("Fiscal Fitness | Home")
-    end
+    it { should have_content('Fiscal Fitness') }
+    it { should have_title('') }
+    it { should_not have_title('| Home') }
   end
 
   describe "Help page" do
+    before { visit help_path }
 
-    it "should have the content 'Help'" do
-      visit '/static_pages/help'
-      expect(page).to have_content('Help')
-    end
-
-    it "should have the title 'Help'" do
-      visit '/static_pages/help'
-      expect(page).to have_title("Fiscal Fitness | Help")
-    end
+    it { should have_content('Help') }
+    it { should have_title(full_title('Help')) }
   end
 
-  describe "About page" do
+  describe "Help page" do
+    before { visit help_path }
 
-    it "should have the content 'About Us'" do
-      visit '/static_pages/about'
-      expect(page).to have_content('About Us')
-    end
-
-    it "should have the title 'About Us'" do
-      visit '/static_pages/about'
-      expect(page).to have_title("Fiscal Fitness | About Us")
-    end
+    it { should have_content('Help') }
+    it { should have_title(full_title('Help')) }
   end
 end

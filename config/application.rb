62af091f8ecc946b9bcdec85b85e173df5ec60fd
Thomas Surgent <surgentt@gmail.com>
2013-11-25 15:35:46 -0500

    updates

--- config/application.rb
+++ config/application.rb
@@ -20,5 +20,13 @@ module FiscalFitness
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
+  
+    # Prevent rails from adding a div to errors messages
+    config.action_view.field_error_proc = Proc.new { |html_tag, instance| 
+      "#{html_tag}".html_safe 
+    }
+
+
   end
+
 end

52c090a491e1dbfe22c93c8f3063b12e6555d89b
Thomas Surgent <surgentt@gmail.com>
2013-11-25 14:18:43 -0500

    email list

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,14 @@
 class UsersController < ApplicationController
   def new
-  	@user = User.new
+    @user = User.new  
   end
 
   def create
     @user = User.new(user_params)
     if @user.save
-      sign_in @user
-    	flash[:success] = "Welcome to Fiscal Fitness!"
-      redirect_to @user
+      #sign_in @user
+    	flash[:success] = "You have been added to our mailing list"
+      redirect_to root_url
     else
       render 'new'
     end
@@ -16,6 +16,10 @@ class UsersController < ApplicationController
 
   def show
   	@user = User.find(params[:id])
+  end
+
+  def index
+    @users = User.all
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ FiscalFitness::Application.routes.draw do
   root  'static_pages#home'
   match '/signup',  to: 'users#new',            via: 'get'
   match '/signin',  to: 'sessions#new',         via: 'get'
+  match '/emails', to: 'users#index',           via: 'get'
   match '/signout', to: 'sessions#destroy',     via: 'delete'
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'

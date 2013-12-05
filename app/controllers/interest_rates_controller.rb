a3a4c3890f8b51b5e1d9c9b7ff25a175f32ca257
Thomas Surgent <surgentt@gmail.com>
2013-12-05 08:28:35 -0500

    auto
    routes updates

--- app/controllers/interest_rates_controller.rb
+++ app/controllers/interest_rates_controller.rb
@@ -1,32 +1,7 @@
 class InterestRatesController < ApplicationController
-  def slide1
-  end
-
-  def slide2
-  end
-
-  def slide3
-  end
-
-  def slide4
-  end
-
-  def slide5
-  end
-
-  def slide6
-  end
-
-  def slide7
-  end
-
-  def slide8
-  end
-
-  def slide9
-  end
-
-  def slide10
+  
+  def show
+    render :template => "interest_rates/slide" + params[:id] + ".html.erb"
   end
 
 end

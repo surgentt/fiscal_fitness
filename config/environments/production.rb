6165470d19daeaeac102436c7eeaa88835b1fc7f
Thomas Surgent <surgentt@gmail.com>
2013-12-05 07:22:01 -0500

    airpair

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,7 +28,8 @@ FiscalFitness::Application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  ## Changed!! - Force Heroku to do the assets precompile
+  config.assets.compile = true
 
   # Generate digests for assets URLs.
   config.assets.digest = true
